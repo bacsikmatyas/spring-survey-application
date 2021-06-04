93ed2920011dc8a8ca5cd001411555235556f34c
Bacsik Mátyás <bacsik.matyas@gmail.com>
2021-06-04 17:46:55 +0200

    Initial commit with generated project

---
+++ src/main/java/hu/unideb/inf/survey/SpringSurveyApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/survey/SpringSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
