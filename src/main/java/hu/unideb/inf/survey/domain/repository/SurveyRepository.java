4eedde4effee1f1bf1f9e6bdaa41f10f1feef5c7
Bacsik Mátyás <bacsik.matyas@gmail.com>
2021-09-04 19:52:54 +0200

    Adding function to find open surveys

--- src/main/java/hu/unideb/inf/survey/domain/repository/SurveyRepository.java
+++ src/main/java/hu/unideb/inf/survey/domain/repository/SurveyRepository.java
@@ -4,7 +4,11 @@ import hu.unideb.inf.survey.domain.entity.Survey;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SurveyRepository extends CrudRepository<Survey, Long> {
     Survey findSurveyByUser_Id(Long id);
+
+    List<Survey> findSurveysByOpen(boolean open);
 }
