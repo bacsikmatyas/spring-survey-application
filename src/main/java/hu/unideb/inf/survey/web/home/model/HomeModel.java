e24953ea737f26680652ded97d0ada58c62cad5b
Bacsik Mátyás <bacsik.matyas@gmail.com>
2021-09-04 19:56:39 +0200

    Creating home model

---
+++ src/main/java/hu/unideb/inf/survey/web/home/model/HomeModel.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.survey.web.home.model;
+
+public class HomeModel {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
