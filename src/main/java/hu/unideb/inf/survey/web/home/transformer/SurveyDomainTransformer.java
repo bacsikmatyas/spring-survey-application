f1f8f429009feac582edaa7579273617df580603
Bacsik Mátyás <bacsik.matyas@gmail.com>
2021-09-04 20:00:06 +0200

    Creating SurveyDomainTransformer

---
+++ src/main/java/hu/unideb/inf/survey/web/home/transformer/SurveyDomainTransformer.java
@@ -0,0 +1,22 @@
+package hu.unideb.inf.survey.web.home.transformer;
+
+import hu.unideb.inf.survey.service.domain.SurveyDomain;
+import hu.unideb.inf.survey.service.domain.UserDomain;
+import hu.unideb.inf.survey.web.home.model.SurveyDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SurveyDomainTransformer {
+    public SurveyDto from(SurveyDomain surveyDomain){
+        SurveyDto surveyDto = new SurveyDto();
+
+        surveyDto.setTitle(surveyDomain.getTitle());
+        surveyDto.setDescription(surveyDomain.getDescription());
+        surveyDto.setType(surveyDomain.getType());
+
+        UserDomain user = surveyDomain.getUser();
+        surveyDto.setAuthorName(user.getName());
+
+        return surveyDto;
+    }
+}
