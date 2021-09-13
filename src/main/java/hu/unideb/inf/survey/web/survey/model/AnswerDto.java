00cc1733b05cabb23e5f3106f19a1b6795322494
Bacsik Mátyás <bacsik.matyas@gmail.com>
2021-09-13 20:30:19 +0200

    Creating AnswerDto and transformer

---
+++ src/main/java/hu/unideb/inf/survey/web/survey/model/AnswerDto.java
@@ -0,0 +1,23 @@
+package hu.unideb.inf.survey.web.survey.model;
+
+public class AnswerDto {
+    private Long id;
+
+    private String answerText;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+}
