f28e71d252589532a8e2181eb1f9ef324554661b
Bacsik Mátyás <bacsik.matyas@gmail.com>
2021-09-19 15:52:45 +0200

    Creating SelectedAnswerService

--- src/main/java/hu/unideb/inf/survey/domain/entity/SelectedAnswer.java
+++ src/main/java/hu/unideb/inf/survey/domain/entity/SelectedAnswer.java
@@ -15,4 +15,37 @@ public class SelectedAnswer {
 
     @ManyToOne
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public QuestionAnswer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(QuestionAnswer answer) {
+        this.answer = answer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "SelectedAnswer{" +
+                "id=" + id +
+                ", answer=" + answer +
+                ", user=" + user +
+                '}';
+    }
 }
